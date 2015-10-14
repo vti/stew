3da5d2b6a601aa51f836101a5bc64ea1e1d257f3
vti <viacheslav.t@gmail.com>
2015-10-14 17:05:46 +0200

    abstract

--- lib/App/stew.pm
+++ lib/App/stew.pm
@@ -9,3 +9,26 @@ our $VERSION = "0.01";
 
 1;
 __END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::stew - stew your binaries!
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Viacheslav Tykhanovskyi, C<vti@cpan.org>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2015, Viacheslav Tykhanovskyi.
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl 5.10.
+
+=cut
