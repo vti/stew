00e14bb521ace66709b077e83a382018ee3ef465
vti <viacheslav.t@gmail.com>
2016-04-16 14:55:22 +0200

    make exec working

--- lib/App/stew/cmd/exec.pm
+++ lib/App/stew/cmd/exec.pm
@@ -4,8 +4,7 @@ use strict;
 use warnings;
 
 use Getopt::Long qw(GetOptionsFromArray);
-use Cwd qw(cwd abs_path);
-use File::Path qw(mkpath);
+use Cwd qw(abs_path);
 use File::Spec;
 use App::stew::env;
 use App::stew::util qw(info debug error);
@@ -28,16 +27,17 @@ sub run {
     my $opt_prefix = 'local';
     GetOptionsFromArray(
         \@argv,
-        "base=s"      => \$opt_base,
-        "prefix=s"    => \$opt_prefix,
+        "base=s"   => \$opt_base,
+        "prefix=s" => \$opt_prefix,
     ) or die "error";
 
     error("--base is required") unless $opt_base;
 
     $opt_base = abs_path($opt_base);
 
-    $ENV{PREFIX} = File::Spec->catfile($opt_base, $opt_prefix);
-    App::stew::env->setup;
+    my $prefix = File::Spec->catfile($opt_base, $opt_prefix);
+
+    App::stew::env->new(prefix => $prefix)->setup;
 
     system(@argv);
 }
